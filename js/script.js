36474106dc795d3e7152d088de7e7209d7de3772
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-19 16:54:54 +0100

    uppercase text

--- js/script.js
+++ js/script.js
@@ -19,29 +19,34 @@
 
 let containerCard = document.querySelector(".containerCard")
 console.log(containerCard);
-let accumulo = [];
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-    .then(res => {
-        res.data.forEach(element => {
-            accumulo.push(element.url)
-        });
-        if (accumulo.length === 6) {
-            for (let i = 0; i < accumulo.length; i++) {
-                containerCard.innerHTML += `<div class="card">
+
+
+generateCard()
+function generateCard() {
+    let indirizzo = [];
+    let testo = [];
+    axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+        .then(res => {
+            res.data.forEach(element => {
+                indirizzo.push(element.url)
+                testo.push(element.title)
+            });
+            if (indirizzo.length === 6 && testo.length === 6) {
+                for (let i = 0; i < indirizzo.length; i++) {
+                    containerCard.innerHTML += `<div class="card">
                 <img src="img/pin.svg" alt="">
                     <div class="imgCard">
-                        <img src="${accumulo[i]}" alt="">
+                        <img src="${indirizzo[i]}" alt="">
                     </div>
-                    <div class="textCard">Lorem ipsum</div>
+                    <div class="textCard">${testo[i][0].toUpperCase() + testo[i].slice(1)}</div>
             </div>`
+                }
             }
-            const card = document.querySelectorAll(".card");
-            console.log(card);
-        }
-    });
 
 
+        });
 
+}
 
 
 
