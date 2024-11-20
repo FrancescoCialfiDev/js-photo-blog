be3ba0d586ec9c264615ab5b750a8be9c12fffc3
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-20 17:13:23 +0100

    formatted some part of the code

--- js/script.js
+++ js/script.js
@@ -1,58 +1,67 @@
-/**
- * 
- * 
- * 
- * 
- * <div class="card">
-<img src="img/pin.svg" alt="">
-<div class="imgCard">
-    <img src="img/cork.png" alt="">
-</div>
-<div class="textCard">Lorem ipsum</div>
-</div>
- * 
- * 
- * 
- * 
- *
- */
+// Dom Loader
 document.addEventListener('DOMContentLoaded', function () {
-    // DOM caricato, inizio manipolazione
+
+    // Variabili Dom Elements
+    const overlay = document.getElementById("overlay");
+    const containerCard = document.querySelector(".containerCard")
+
+    // Variabili Button Function
+    const btnElimina = document.getElementById("elimina")
+    const btnchiudi = document.getElementById("chiudi")
 
 
-    let containerCard = document.querySelector(".containerCard")
-    console.log(containerCard);
 
 
     async function generateCard() {
         await axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
             .then(res => {
                 res.data.forEach(element => {
-                    containerCard.innerHTML += `<div class="card">
-                    <img id="${element.id}" class="pin" src="img/pin.svg" alt="">
+                    containerCard.innerHTML += `<div id="${element.id}" class="card">
+                    <img class="pin" src="img/pin.svg" alt="">
                         <div class="imgCard">
                             <img src="${element.url}" alt="">
                         </div>
-                        <div class="textCard">${element.title[0].toUpperCase() + element.title.slice(1)}</div>
+                        <div class="textCard">${element.title}</div>
                 </div>`
                 });
-
             });
-    }
+    };
+
 
     createApp();
     async function createApp() {
         await generateCard();
-        let nodeList = document.querySelectorAll(".card .imgCard img");
-        nodeList.forEach(element => {
+        const imgNodelist = document.querySelectorAll(".card .imgCard img")
+        console.log(imgNodelist) // NodeList Immagini con Indice
+        const cardNodelist = document.querySelectorAll(".card")
+        console.log(cardNodelist); // Contieni ID Card
+
+
+        imgNodelist.forEach(element => {
             element.addEventListener("click", function () {
-                let overlay = document.getElementById("overlay");
-                overlay.classList.remove("d-none");
-                let imgDnone = document.querySelector("#overlay img")
-                imgDnone.src = element.src
+                overlay.classList.remove("d-none"); // Remove d-none class, Show content Overlay.
+                let imgDnone = document.querySelector("#overlay img");
+                imgDnone.src = element.src; // Change source of overlay img and sets it equal to the card.
+
             })
         });
+
     };
+
+
+
+    btnchiudi.addEventListener("click", function () {
+        overlay.classList.add("d-none")
+    });
+
+    btnElimina.addEventListener("click", function () {
+        overlay.classList.add("d-none")
+
+    });
+
+
+
+
 
 
 });
