220b9c599cd6881010120575fc02ad380985a70d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-11-20 13:32:55 +0100

    added click function and removed some thing by generate card

--- js/script.js
+++ js/script.js
@@ -25,33 +25,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     async function generateCard() {
-        let indirizzo = [];
-        let testo = [];
         await axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
             .then(res => {
                 res.data.forEach(element => {
-                    indirizzo.push(element.url)
-                    testo.push(element.title)
-                });
-                if (indirizzo.length === 6 && testo.length === 6) {
-                    for (let i = 0; i < indirizzo.length; i++) {
-                        containerCard.innerHTML += `<div class="card">
-                    <img class="pin" src="img/pin.svg" alt="">
+                    containerCard.innerHTML += `<div class="card">
+                    <img  class="pin" src="img/pin.svg" alt="">
                         <div class="imgCard">
-                            <img src="${indirizzo[i]}" alt="">
+                            <img src="${element.url}" alt="">
                         </div>
-                        <div class="textCard">${testo[i][0].toUpperCase() + testo[i].slice(1)}</div>
+                        <div class="textCard">${element.title[0].toUpperCase() + element.title.slice(1)}</div>
                 </div>`
-                    }
-                }
-
+                });
             });
     }
 
     createApp();
     async function createApp() {
         await generateCard();
-        console.log(document.querySelectorAll(".card"))
+        let nodeList = document.querySelectorAll(".card");
+        console.log(nodeList);
+        nodeList.forEach(element => {
+
+            element.addEventListener("click", function () {
+                let overlay = document.getElementById("overlayImg")
+                overlay.classList.remove("d-none")
+
+            })
+        });
     }
 
 
